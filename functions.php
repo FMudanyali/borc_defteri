b72e5c4a2b29862794fd267728a420f15296d194
fmudanyali <furkan@Market.localdomain>
2019-08-09 18:32:38 +0300

    done

--- functions.php
+++ functions.php
@@ -45,7 +45,6 @@ function totalBalance($db,$customer){
 function listPeople($db){
     echo '<table id="myTable">';
     // To calculate total amount of loan.
-    $total_loan = 0;
     $tablesquery = $db->query("SELECT name FROM sqlite_master WHERE type='table';");
     while ($table = $tablesquery->fetchArray(SQLITE3_ASSOC)) {
         // Ignore this table
@@ -56,7 +55,6 @@ function listPeople($db){
             .'<td><a href="edit.php?value='."$customer"
             .'"><div id="diva">'.$customer.'</div></a></td><td style="text-align:right;">'
             .number_format($customerBalance,2).' TL</td>'."</tr>";
-            $total_loan = $total_loan + $customerBalance;
         }
     }
     echo "</table><script>sortTable()</script>";
@@ -68,4 +66,16 @@ function listPeople($db){
     echo "</table>";
     
 }
+function toplamAlacak($db){
+    $total_loan = 0;
+    $tablesquery = $db->query("SELECT name FROM sqlite_master WHERE type='table';");
+    while ($table = $tablesquery->fetchArray(SQLITE3_ASSOC)) {
+        if ($table['name']!="sqlite_sequence") {
+            $customer = $table['name'];
+            $customerBalance = totalBalance($db,$customer);
+            $total_loan = $total_loan + $customerBalance;
+        }
+    }
+    echo "Toplam Alacak: ".number_format($total_loan,2)." TL";  
+} 
 ?>
--- index.php
+++ index.php
@@ -4,12 +4,14 @@
   <link rel="stylesheet" href="styles.css">
   <script type="text/javascript" src="jquery.js"></script>
   <script type="text/javascript" src="functions.js"></script>
+  <?php include("functions.php")?>
 </head>
 <body>
 <table width=100% style="text-align:center;margin-top:300px;">
 <tr><td><a href=customers.php><div id=diva>Hesap Görüntüle</div></a></td></tr>
 <tr><td><a href=add_customer.php><div id=diva>Müşteri Ekle</div></a></td></tr>
 <tr><td><a href=remove_customer.php><div id=diva>Müşteri Sil</div></a></td></tr>
+<tr><td><><div id=diva><?php toplamAlacak(); ?></div></a></td></tr>
 </table>
 </body>
 </html>
